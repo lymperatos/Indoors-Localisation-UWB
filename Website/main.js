079baad0c1dc8e18583c131f6718acf2c04d018d
Dimitris <dimitrislimperatos@gmail.com>
2020-11-12 00:27:04 +0200

    Update main.js

--- Website/main.js
+++ Website/main.js
@@ -20,9 +20,9 @@ firebase.auth().onAuthStateChanged(function(user) {
     window.location = "https://lymperatos.com/Website/locate.html";//TODO: FIX THIS
 
   } else {
-    if(window.location != "index.html"){
-      window.location = "https://lymperatos.com";
-    }
+    // if(window.location != "index.html"){
+    //   window.location = "https://lymperatos.com";
+    // }
 
 
   }
