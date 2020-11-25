5f951187cae45a697712e3fca0855bfeada959b4
Dimitris Lymperatos <dimitrislimperatos@gmail.com>
2020-11-25 20:57:06 +0000

    added credentials

--- ESP32/Tag/Tag.ino
+++ ESP32/Tag/Tag.ino
@@ -2,6 +2,7 @@
 #include "DW1000Ranging.h"
 #include <WiFi.h>
 #include "FirebaseESP32.h"
+#include "credentials.h"
 
 /* Pins connecting DWM1000 */
 const uint8_t PIN_SCK = 18;
@@ -9,7 +10,7 @@ const uint8_t PIN_MOSI = 23;
 const uint8_t PIN_MISO = 19;
 const uint8_t PIN_SS = 2;
 const uint8_t PIN_RST = 15;
-const uint8_t PIN_IRQ = 17; 
+const uint8_t PIN_IRQ = 17;
 
 /* Wifi setup */
 #define WIFI_SSID "";
@@ -21,16 +22,17 @@ const uint8_t PIN_IRQ = 17;
 //#define databaseURL "https://****.com";
 //#define secret "****";
 
+
 FirebaseData firebaseData;
-Firebase.begin(databaseURL, secret);
-Firebase.reconnectWifi(true);
-Firebase.setMaxRetry(firebaseData,3);
+
+
 String folder = "";
 
 
 void setup() {
   Serial.begin(115200);
   delay(1000);
+
   //init the configuration
   DW1000Ranging.initCommunication(PIN_RST, PIN_SS, PIN_IRQ); //Reset, CS, IRQ pin
   //define the sketch as anchor. It will be great to dynamically change the type of module
@@ -40,16 +42,19 @@ void setup() {
   //Enable the filter to smooth the distance
   DW1000Ranging.useRangeFilter(true);
 
-    DW1000.enableDebounceClock();
+  DW1000.enableDebounceClock();
   DW1000.enableLedBlinking();
 
   DW1000.setGPIOMode(MSGP0, LED_MODE);
   DW1000.setGPIOMode(MSGP2, LED_MODE);
   DW1000.setGPIOMode(MSGP3, LED_MODE);
-  
+
   //we start the module as a tag
   DW1000Ranging.startAsTag("7D:00:22:EA:82:60:3B:9C", DW1000.MODE_LONGDATA_RANGE_ACCURACY);
 
+
+  /* Firebase start */
+  Firebase.begin(databaseURL, secret);
 }
 
 void loop() {
@@ -58,7 +63,7 @@ void loop() {
 
 void newRange() {
  // Serial.print("from: "); Serial.print(DW1000Ranging.getDistantDevice()->getShortAddress(), HEX);
-  Serial.print("\t Range: "); 
+  Serial.print("\t Range: ");
   Serial.println(DW1000Ranging.getDistantDevice()->getRange()); //Serial.print(" m");
   //Serial.print("\t RX power: "); Serial.print(DW1000Ranging.getDistantDevice()->getRXPower()); Serial.println(" dBm");
 }
