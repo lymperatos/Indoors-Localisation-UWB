d5d223854a604b35109b113e9ffbbefad8c77012
Dimitris Lymperatos <dimitrislimperatos@gmail.com>
2020-11-25 21:29:36 +0000

    Update Tag.ino

--- ESP32/Tag/Tag.ino
+++ ESP32/Tag/Tag.ino
@@ -13,8 +13,9 @@ const uint8_t PIN_RST = 15;
 const uint8_t PIN_IRQ = 17;
 
 /* Wifi setup */
-#define WIFI_SSID "";
-#define WIFI_PASSWORD "";
+//#define WIFI_SSID "****";
+//#define WIFI_PASSWORD "****";
+
 
 
 /* Firebase Setup */
@@ -26,20 +27,18 @@ const uint8_t PIN_IRQ = 17;
 FirebaseData firebaseData;
 
 
-String folder = "";
+String folder = "/Tag";
 
 
 void setup() {
   Serial.begin(115200);
   delay(1000);
 
-  //init the configuration
+  /* DW1000 Connection */
   DW1000Ranging.initCommunication(PIN_RST, PIN_SS, PIN_IRQ); //Reset, CS, IRQ pin
-  //define the sketch as anchor. It will be great to dynamically change the type of module
   DW1000Ranging.attachNewRange(newRange);
   DW1000Ranging.attachNewDevice(newDevice);
   DW1000Ranging.attachInactiveDevice(inactiveDevice);
-  //Enable the filter to smooth the distance
   DW1000Ranging.useRangeFilter(true);
 
   DW1000.enableDebounceClock();
@@ -49,23 +48,38 @@ void setup() {
   DW1000.setGPIOMode(MSGP2, LED_MODE);
   DW1000.setGPIOMode(MSGP3, LED_MODE);
 
-  //we start the module as a tag
+
   DW1000Ranging.startAsTag("7D:00:22:EA:82:60:3B:9C", DW1000.MODE_LONGDATA_RANGE_ACCURACY);
 
+  /* Wifi connection */
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  while(WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(300);
+  }
+  Serial.println("");
 
-  /* Firebase start */
+  /* Firebase connection */
   Firebase.begin(databaseURL, secret);
+ 
 }
 
 void loop() {
   DW1000Ranging.loop();
 }
 
+float prev = 0.0;
 void newRange() {
  // Serial.print("from: "); Serial.print(DW1000Ranging.getDistantDevice()->getShortAddress(), HEX);
   Serial.print("\t Range: ");
   Serial.println(DW1000Ranging.getDistantDevice()->getRange()); //Serial.print(" m");
   //Serial.print("\t RX power: "); Serial.print(DW1000Ranging.getDistantDevice()->getRXPower()); Serial.println(" dBm");
+  
+  if(DW1000Ranging.getDistantDevice()->getRange() != prev){
+    prev = DW1000Ranging.getDistantDevice()->getRange();
+    Firebase.setFloat(firebaseData,folder + "/A1", DW1000Ranging.getDistantDevice()->getRange());
+  }
+  
 }
 
 void newDevice(DW1000Device* device) {
