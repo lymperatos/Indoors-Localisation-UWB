a21497430bb0bd429cb140e09879fa95b7fed919
lymperatos <dimitrislimperatos@gmail.com>
2020-12-22 17:02:42 +0000

    Working Anchors and Tags
    e

--- ESP32/Anchor/Anchor.ino
+++ ESP32/Anchor/Anchor.ino
@@ -1,6 +1,10 @@
 #include <SPI.h>
 #include "DW1000Ranging.h"
 
+#include <WiFi.h>
+#include "FirebaseESP32.h"
+#include "credentials.h"
+
 const uint8_t PIN_SCK = 18;
 const uint8_t PIN_MOSI = 23;
 const uint8_t PIN_MISO = 19;
@@ -8,17 +12,25 @@ const uint8_t PIN_SS = 2;
 const uint8_t PIN_RST = 15;
 const uint8_t PIN_IRQ = 17;
 
+const int tLed = 14;
+
+FirebaseData firebaseData;
+String folder = "/users/eEPjvQNNSvQODEAwq31YdRrRvCi2/";
+
 void setup() {
   Serial.begin(115200);
   delay(1000);
+
+    startWifi();      /* Wifi connection */
+  startFirebase();  /* Firebase connection */ 
+  
   //init the configuration
   DW1000Ranging.initCommunication(PIN_RST, PIN_SS, PIN_IRQ); //Reset, CS, IRQ pin
   //define the sketch as anchor. It will be great to dynamically change the type of module
   DW1000Ranging.attachNewRange(newRange);
   DW1000Ranging.attachBlinkDevice(newBlink);
   DW1000Ranging.attachInactiveDevice(inactiveDevice);
-  //Enable the filter to smooth the distance
-  DW1000Ranging.useRangeFilter(true);
+  DW1000Ranging.useRangeFilter(false);
 
   
   DW1000.enableDebounceClock();
@@ -27,24 +39,35 @@ void setup() {
   DW1000.setGPIOMode(MSGP0, LED_MODE);
   DW1000.setGPIOMode(MSGP2, LED_MODE);
   DW1000.setGPIOMode(MSGP3, LED_MODE);
+
+   pinMode(tLed, OUTPUT);
   
   //we start the module as an anchor
   DW1000Ranging.startAsAnchor("82:17:5B:D5:A9:9A:E2:9C", DW1000.MODE_LONGDATA_RANGE_ACCURACY);
-
+  //DW1000Ranging.startAsAnchor("83:17:5B:D5:A9:9A:E2:9C", DW1000.MODE_LONGDATA_RANGE_ACCURACY);
 
  
 
 
 }
-
+int period = 5000;
+unsigned long time_now = 0;
+float d1 = 0;
 void loop() {
   DW1000Ranging.loop();
+//  digitalWrite(tLed, LOW);
+//   if(millis() >= time_now + period){
+//        time_now += period;
+//        Serial.println("saved");
+//      //  Firebase.setFloat(firebaseData, folder+"Devices/Anchor 1/dist", d1);
+//    }
+    
 }
 
 void newRange() {
-  Serial.print("from: "); Serial.print(DW1000Ranging.getDistantDevice()->getShortAddress(), HEX);
-  Serial.print("\t Range: "); Serial.print(DW1000Ranging.getDistantDevice()->getRange()); Serial.print(" m");
-  Serial.print("\t RX power: "); Serial.print(DW1000Ranging.getDistantDevice()->getRXPower()); Serial.println(" dBm");
+   d1 = DW1000Ranging.getDistantDevice()->getRange();
+ 
+  
 }
 
 void newBlink(DW1000Device* device) {
@@ -56,4 +79,19 @@ void newBlink(DW1000Device* device) {
 void inactiveDevice(DW1000Device* device) {
   Serial.print("delete inactive device: ");
   Serial.println(device->getShortAddress(), HEX);
+}
+
+void startWifi(){
+   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  while(WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    digitalWrite(tLed, HIGH);
+    delay(300);
+    digitalWrite(tLed, LOW);
+  }
+  Serial.println("");
+}
+
+void startFirebase(){
+  Firebase.begin(databaseURL, secret);
 }
--- ESP32/Tag/Tag.ino
+++ ESP32/Tag/Tag.ino
@@ -12,6 +12,57 @@ const uint8_t PIN_SS = 2;
 const uint8_t PIN_RST = 15;
 const uint8_t PIN_IRQ = 17;
 
+const int tLed = 14;
+
+
+const int a1 = 6018; 
+const int a2 = 6019; 
+const int a3 = 6020; 
+const int a4 = 6021; 
+
+float d1,d2,d3,d4 = 0;
+
+
+
+typedef union {
+ float floatingPoint;
+ byte binary[4];
+ unsigned long ulong;
+} binaryFloat;
+
+
+void printFloat(float v){
+  binaryFloat b;
+  b.floatingPoint = v;  
+  Serial.write(b.binary[0]);  
+  Serial.write(b.binary[1]);  
+  Serial.write(b.binary[2]);  
+  Serial.write(b.binary[3]);  
+}
+
+void printLong(unsigned long v){
+  binaryFloat b;
+  b.ulong = v; 
+  Serial.write(b.binary[0]);  
+  Serial.write(b.binary[1]);  
+  Serial.write(b.binary[2]);  
+  Serial.write(b.binary[3]);  
+}
+
+void showDeviceInfo(){
+    // DEBUG chip info and registers pretty printed
+    char msg[256];
+    DW1000.getPrintableDeviceIdentifier(msg);
+    Serial.print("Device ID: "); Serial.println(msg);
+    DW1000.getPrintableExtendedUniqueIdentifier(msg);
+    Serial.print("Unique ID: "); Serial.println(msg);
+    DW1000.getPrintableNetworkIdAndShortAddress(msg);
+    Serial.print("Network ID & Device Address: "); Serial.println(msg);
+    DW1000.getPrintableDeviceMode(msg); 
+    Serial.print("Device mode: "); Serial.println(msg);
+}
+
+
 /* Wifi setup */
 //#define WIFI_SSID "****";
 //#define WIFI_PASSWORD "****";
@@ -20,10 +71,10 @@ const uint8_t PIN_IRQ = 17;
 //#define databaseURL "https://****.com";
 //#define secret "****";
 FirebaseData firebaseData;
-String folder = "/Tag";
+String folder = "/users/eEPjvQNNSvQODEAwq31YdRrRvCi2/";
 
 TaskHandle_t Task1;
-float prev = 0.0;
+
 
 void setup() {
   Serial.begin(115200);
@@ -33,34 +84,73 @@ void setup() {
   startWifi();      /* Wifi connection */
   startFirebase();  /* Firebase connection */ 
 
+  pinMode(tLed, OUTPUT);
+  digitalWrite(tLed, LOW);
 
-  xTaskCreatePinnedToCore(
-                    Task1code,   /* Task function. */
-                    "Task1",     /* name of task. */
-                    10000,       /* Stack size of task */
-                    NULL,        /* parameter of the task */
-                    1,           /* priority of the task */
-                    &Task1,      /* Task handle to keep track of created task */
-                    0);          /* pin task to core 0 */                  
-  delay(500); 
+
+//  xTaskCreatePinnedToCore(
+//                    Task1code,   /* Task function. */
+//                    "DW loop",     /* name of task. */
+//                    10000,       /* Stack size of task */
+//                    NULL,        /* parameter of the task */
+//                    1,           /* priority of the task */
+//                    &Task1,      /* Task handle to keep track of created task */
+//                    0);          /* pin task to core 0 */                  
+//  delay(500); 
+
+//   xTaskCreatePinnedToCore(
+//                    Task2code,   /* Task function. */
+//                    "Uploading to firebase",     /* name of task. */
+//                    10000,       /* Stack size of task */
+//                    NULL,        /* parameter of the task */
+//                    1,           /* priority of the task */
+//                    &Task1,      /* Task handle to keep track of created task */
+//                    1);          /* pin task to core 0 */     
 
 }
- 
+
 /* Core 0 loop */
-void Task1code( void * pvParameters ){
-    for(;;){
-      loop0();
-    } 
-}
+int period = 1000;
+unsigned long time_now = 0;
+
+//void Task1code( void * pvParameters ){
+// digitalWrite(tLed, HIGH);
+//  Serial.println(xPortGetCoreID());
+// for(;;){
+//       
+//
+//    
+//    delay(30);
+//    
+//         
+// }
+//    
+//}
+
+
+
 
 /* Core 0 loop*/
-void loop0(){
-  Firebase.setFloat(firebaseData, folder+"/A1", prev);
-    delay(1000);
-}
+//void loop0(){
+//
+//  Firebase.setFloat(firebaseData, folder+"Devices/Anchor 1/dist", d1);
+//  Firebase.setFloat(firebaseData, folder+"Devices/Anchor 2/dist", d2);
+// 
+//  //Firebase.setFloat(firebaseData, folder+"Devices/Anchor 3/dist", d3);
+//  
+//}
 /* Core 1 loop*/
 void loop() {
-  DW1000Ranging.loop();
+    
+  
+    DW1000Ranging.loop();
+
+     if(millis() >= time_now + period){
+        time_now += period;
+           Firebase.setFloat(firebaseData, folder+"Devices/Anchor 1/dist", d1);
+    Firebase.setFloat(firebaseData, folder+"Devices/Anchor 2/dist", d2);
+    }
+ 
 }
 
 
@@ -68,7 +158,9 @@ void startWifi(){
    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   while(WiFi.status() != WL_CONNECTED){
     Serial.print(".");
+    digitalWrite(tLed, HIGH);
     delay(300);
+    digitalWrite(tLed, LOW);
   }
   Serial.println("");
 }
@@ -76,45 +168,90 @@ void startWifi(){
 void startDWM1000(){
   DW1000Ranging.initCommunication(PIN_RST, PIN_SS, PIN_IRQ); //Reset, CS, IRQ pin
   DW1000Ranging.attachNewRange(newRange);
-  DW1000Ranging.attachNewDevice(newDevice);
-  DW1000Ranging.attachInactiveDevice(inactiveDevice);
-  DW1000Ranging.useRangeFilter(true);
+ // DW1000Ranging.attachNewDevice(newDevice);
+  //DW1000Ranging.attachInactiveDevice(inactiveDevice);
+  //DW1000Ranging.useRangeFilter(false);
+//
+//  DW1000.enableDebounceClock();
+//  DW1000.enableLedBlinking();
+//
+//  DW1000.setGPIOMode(MSGP0, LED_MODE);
+//  DW1000.setGPIOMode(MSGP2, LED_MODE);
+//  DW1000.setGPIOMode(MSGP3, LED_MODE);
 
-  DW1000.enableDebounceClock();
-  DW1000.enableLedBlinking();
 
-  DW1000.setGPIOMode(MSGP0, LED_MODE);
-  DW1000.setGPIOMode(MSGP2, LED_MODE);
-  DW1000.setGPIOMode(MSGP3, LED_MODE);
-
-
-  DW1000Ranging.startAsTag("7D:00:22:EA:82:60:3B:9C", DW1000.MODE_LONGDATA_RANGE_ACCURACY);
+  DW1000Ranging.startAsTag("00:00:22:EA:82:60:3B:9C", DW1000.MODE_LONGDATA_RANGE_ACCURACY);
 }
 
 void startFirebase(){
   Firebase.begin(databaseURL, secret);
 }
 
-void newRange() {
- // Serial.print("from: "); Serial.print(DW1000Ranging.getDistantDevice()->getShortAddress(), HEX);
-  Serial.print("\t Range: ");
-  Serial.println(DW1000Ranging.getDistantDevice()->getRange()); //Serial.print(" m");
-  //Serial.print("\t RX power: "); Serial.print(DW1000Ranging.getDistantDevice()->getRXPower()); Serial.println(" dBm");
+//void newRange() {
+//  if(DW1000Ranging.getDistantDevice()->getShortAddress() == a1){
+//  d1 = DW1000Ranging.getDistantDevice()->getRange();
+//  if(d1 >= 0.01){
+//  Firebase.setFloat(firebaseData, folder+"Devices/Anchor 1/dist", d1);
+//  }
+//  }else{
+//     d1 = DW1000Ranging.getDistantDevice()->getRange();
+//    Firebase.setFloat(firebaseData, folder+"Devices/Anchor 2/dist", d1);
+//  }
+//}
+
+float dist,power = 0;
+void newRange(){ 
+  float d = DW1000Ranging.getDistantDevice()->getRange();
+  float p = DW1000Ranging.getDistantDevice()->getRXPower();
+  unsigned int a = DW1000Ranging.getDistantDevice()->getShortAddress();
+
+//  int idx = (a >> 8) - 49;
+//  if (idx == 0) d1 = d;
+//  if (idx == 1) d2 = d;
+//  Serial.print(d1);
+//  Serial.print("  ");
+//  Serial.println(d2);
   
-  if(DW1000Ranging.getDistantDevice()->getRange() != prev){
-    prev = DW1000Ranging.getDistantDevice()->getRange();
-    
+//  Serial.write(0x44);
+//  Serial.write(0xaa); 
+//  printLong(millis());
+//  Serial.write(a >> 8);
+//  Serial.write(a & 0xFF);
+//  printFloat(d);
+//  printFloat(p);  
+//  Serial.write(0x44);
+//  Serial.write(0xaa);  
+  if(DW1000Ranging.getDistantDevice()->getShortAddress() == a1){
+  Serial.print("from: "); Serial.print(DW1000Ranging.getDistantDevice()->getShortAddress(), HEX);  
+  Serial.print("\t Range: "); Serial.print(DW1000Ranging.getDistantDevice()->getRange()); Serial.print(" m"); 
+  Serial.print("\t RX power: "); Serial.print(DW1000Ranging.getDistantDevice()->getRXPower()); Serial.println(" dBm");
+ d1= DW1000Ranging.getDistantDevice()->getRange();
+  }else{
+    Serial.print("from: "); Serial.print(DW1000Ranging.getDistantDevice()->getShortAddress(), HEX);  
+  Serial.print("\t Range: "); Serial.print(DW1000Ranging.getDistantDevice()->getRange()); Serial.print(" m"); 
+  Serial.print("\t RX power: "); Serial.print(DW1000Ranging.getDistantDevice()->getRXPower()); Serial.println(" dBm");
+  d2= DW1000Ranging.getDistantDevice()->getRange();
   }
-  
+//  
+//  dist = 0.9 * dist + 0.1 * d;
+//  power = 0.9* power + 0.1 * p;
+//  Serial.print("NAme: ");
+//  Serial.print(a);
+//  Serial.print("distance: ");
+//  Serial.print(dist);
+//  Serial.print(" m");
+//  Serial.print("  power: ");
+//  Serial.print(power);
+//  Serial.println(" dBm");
 }
 
 void newDevice(DW1000Device* device) {
   Serial.print("ranging init; 1 device added ! -> ");
   Serial.print(" short:");
-  //Serial.println(device->getShortAddress(), HEX);
+  Serial.println(device->getShortAddress(), HEX);
 }
 
 void inactiveDevice(DW1000Device* device) {
   Serial.print("delete inactive device: ");
- // Serial.println(device->getShortAddress(), HEX);
+  Serial.println(device->getShortAddress(), HEX);
 }
